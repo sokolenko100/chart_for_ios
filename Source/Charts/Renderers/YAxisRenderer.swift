b05dc89c0219fda9ed9d67fb9a8f33fcc24b9a05
artemsokol <artems@123software.ru>
2019-11-23 16:02:23 +0200

    'change_YAxisRenderer'

--- Source/Charts/Renderers/YAxisRenderer.swift
+++ Source/Charts/Renderers/YAxisRenderer.swift
@@ -124,6 +124,7 @@ open class YAxisRenderer: AxisRendererBase
         context.restoreGState()
     }
     
+    
     /// draws the y-labels on the specified x-position
     internal func drawYLabels(
         context: CGContext,
@@ -142,6 +143,15 @@ open class YAxisRenderer: AxisRendererBase
         let from = yAxis.isDrawBottomYLabelEntryEnabled ? 0 : 1
         let to = yAxis.isDrawTopYLabelEntryEnabled ? yAxis.entryCount : (yAxis.entryCount - 1)
         
+        let limitLines = yAxis.limitLines //вытянули масив линий
+        
+        var yOffset = CGFloat(0.0)
+        
+        if limitLines.count == 0
+        {
+            yOffset = CGFloat(-6.0)
+        }
+        
         for i in stride(from: from, to: to, by: 1)
         {
             let text = yAxis.getFormattedLabel(i)
@@ -151,10 +161,135 @@ open class YAxisRenderer: AxisRendererBase
                 text: text,
                 point: CGPoint(x: fixedPosition, y: positions[i].y + offset),
                 align: textAlign,
-                attributes: [.font: labelFont, .foregroundColor: labelTextColor]
-            )
+                attributes: [NSAttributedString.Key.font: labelFont, NSAttributedString.Key.foregroundColor: labelTextColor])
         }
+        
+        renderIndicatorBlock(
+            context: context,
+            fixedPosition: fixedPosition,
+            positions: transformedPositions(),
+            textAlign: textAlign)
     }
+    
+    
+    internal func renderIndicatorBlock( context: CGContext,
+                                        fixedPosition: CGFloat,
+                                        positions: [CGPoint],
+                                        textAlign: NSTextAlignment)
+    {
+        guard
+            let yAxis = self.axis as? YAxis
+            else { return }
+        
+        //var [CGPoint] positions = transformedPositions() // получили значение по оси У
+        var limitLines = yAxis.limitLines //вытянули масив линий
+        
+        if limitLines.count == 0
+        {
+            return
+        }
+        
+        
+        let l = limitLines[0] // вытянули одну линию
+        let blockLimitNumber = CGFloat(l.limit) // получили значение лимит блока
+        
+        let limitFirstText = yAxis.getFormattedLabel(1)  //получили значение по осям У в формате текста
+        let limitSecondText = yAxis.getFormattedLabel(2)  //получили значение по осям У в формате текста
+        
+        // let labelTextColor = yAxis.labelTextColor // вытянули цвет текста
+        let labelFont = yAxis.labelFont // вытянули размер текста
+        
+        // let limitFirst = getCGFloat(from: limitFirstText) // нужно преобразовать в число значение оси
+        let gradeFirst = positions[1].y
+        
+        // let limitSecond = getCGFloat(from:limitSecondText) // нужно преобразовать в число значение оси
+        let gradeSecond = positions[2].y
+        
+        let limitFirstChanged = String(format:"%.5f", limitFirstText.doubleValue)
+        let limitFirst = getCGFloat(from: limitFirstChanged)
+        
+        let limitSecondChanged = String(format:"%.5f", limitSecondText.doubleValue)
+        let limitSecond = getCGFloat(from: limitSecondChanged)
+        
+        let limitDifferent: Float = Float(limitSecond ?? 0.0) - Float(limitFirst ?? 0.0)
+        let gradeDifferent: Float = Float(gradeFirst ) - Float(gradeSecond )
+        
+        let different = Float(blockLimitNumber ) - Float(limitSecond ?? 0.0)
+        let differentRangeFromZero = ((different * gradeDifferent) / limitDifferent)
+        let positionYFloat = Float(gradeSecond) - differentRangeFromZero //задали позицию Y
+        let positionYText = CGFloat(positionYFloat-6)
+        let positionY = CGFloat(positionYFloat-10)
+        
+        var components = limitLines[0].lineColor.cgColor.components!
+        
+        let rectColor = UIColor(red: CGFloat(components[0]),
+                                green: CGFloat(components[1]),
+                                blue: CGFloat(components[2]),
+                                alpha: CGFloat(components[3]))
+        
+        let labelTextColor = UIColor(red: CGFloat(0),
+                                     green: CGFloat(0),
+                                     blue: CGFloat(0),
+                                     alpha: CGFloat(1))
+        
+        drawRect(pointX: CGFloat(fixedPosition),
+                 pointY: CGFloat(positionY),
+                 context: UIGraphicsGetCurrentContext()!,
+                 strokeColor: rectColor,
+                 fillColor: rectColor)
+        
+        ChartUtils.drawText(
+            context: context,
+            text: getString(from: CGFloat(l.limit)),
+            point: CGPoint(x: fixedPosition-2, y: positionYText),
+            align: textAlign,
+            attributes: [NSAttributedString.Key.font: labelFont,  NSAttributedString.Key.foregroundColor: labelTextColor])
+    }
+    
+    //Method that you need to parse string to cgfloat
+    private func getCGFloat(from string: String) -> CGFloat?{
+        if let double = Double(string){
+            return CGFloat(double)
+        }
+        
+        return nil
+    }
+    
+    private func getString(from cgFloat: CGFloat?) -> String{
+        if let float = cgFloat{
+            return float.description
+        }
+        
+        return "nil"
+    }
+    
+    private func getDouble(from string: String) -> Double?{
+        if let double = Double(string){
+            return double
+        }
+        return nil
+    }
+    
+    //Method that you need to draw rectangle
+    //If you have colors in another format (CGColor, RGB etc.. just conver them to UIColor)
+    private func drawRect(pointX: CGFloat,pointY: CGFloat, context: CGContext, strokeColor: UIColor, fillColor: UIColor){
+        let path = UIBezierPath()
+        
+        path.move(to: CGPoint(x:pointX-10.0, y: pointY+10))
+        path.addLine(to: CGPoint(x: pointX-4.0, y: pointY))
+        path.addLine(to: CGPoint(x: pointX+46.0, y: pointY))
+        path.addLine(to: CGPoint(x: pointX+46.0, y: pointY+20))
+        path.addLine(to: CGPoint(x: pointX-4.0, y: pointY+20))
+        path.addLine(to: CGPoint(x:pointX-10.0, y: pointY+10))
+        path.close()
+        
+        let bezierPath = path.cgPath
+        fillColor.setFill()
+        strokeColor.setStroke()
+        context.addPath(bezierPath)
+        context.drawPath(using: .fillStroke)
+    }
+    
     
     open override func renderGridLines(context: CGContext)
     {
@@ -390,5 +525,49 @@ open class YAxisRenderer: AxisRendererBase
         }
         
         context.restoreGState()
+    }
+}
+
+extension UIColor {
+    public convenience init?(hexString: String) {
+        let r, g, b, a: CGFloat
+        
+        if hexString.hasPrefix("#") {
+            let start = hexString.index(hexString.startIndex, offsetBy: 1)
+            let hexColor = String(hexString[start...])
+            
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+                
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+                    
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        
+        return nil
+    }
+}
+
+extension String {
+    static let numberFormatter = NumberFormatter()
+    var doubleValue: Double {
+        String.numberFormatter.decimalSeparator = "."
+        if let result =  String.numberFormatter.number(from: self) {
+            return result.doubleValue
+        } else {
+            String.numberFormatter.decimalSeparator = ","
+            if let result = String.numberFormatter.number(from: self) {
+                return result.doubleValue
+            }
+        }
+        return 0
     }
 }
